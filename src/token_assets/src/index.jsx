e5a17e6cc12b15f2a93cdfded8af69728cfa88a4
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-01-18 09:50:04 +0100

    add authClient

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
 
 const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  
+
+  const authClient = await AuthClient.create();
+
+  await authClient.login({
+    identityProvider: "https://identity.ic0.app/#authorize",
+    onSuccess: () => {
+      ReactDOM.render(<App />, document.getElementById("root"));
+    }
+  })
 }
 
 init();
